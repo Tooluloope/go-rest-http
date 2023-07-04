61773f6ca2fe9770772c8c7f5ea879a6ee28ae17
Tolulope <adetulatolu@gmail.com>
2023-07-04 12:27:15 +0100

    feat: dockerize and introduce postgres

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/tooluloope/go-rest-http/internal/db"
+)
 
 // Run Func would instantiate and startup the server
 
 func Run() error {
 	fmt.Println("Starting server...")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Error connecting to db")
+		return err
+	}
+
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+
+	fmt.Println("Connected to db and Pinged successfully")
 	return nil
 }
 
